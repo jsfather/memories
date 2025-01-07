ade5f81c0d7e41d71a042b727fec1fb24e60809d
jsfather <keyvanmatinrad@outlook.com>
2025-01-07 22:03:25 +0330

    SearchForm added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,13 @@
+import SearchForm from '@/app/components/SearchForm';
+
 export default function Home() {
-  return <>HOME</>;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Share your memories</h1>
+        <p className="sub-heading !max-w-3xl">Submit your memory</p>
+        <SearchForm />
+      </section>
+    </>
+  );
 }
